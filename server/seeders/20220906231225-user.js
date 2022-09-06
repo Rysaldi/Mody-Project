9fcc485b82b1da55dba37f7fb854284a3cfca574
inyund <mfrafie@gmail.com>
2022-09-07 06:32:36 +0700

    create seeder for user data

--- server/seeders/20220906231225-user.js
+++ server/seeders/20220906231225-user.js
@@ -1,8 +1,14 @@
 "use strict";
 const userData = require("../data/user.json");
+const { hashPassword } = require("../helpers/bcrypt");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const data = userData.map((user) => {
+      user.password = hashPassword(user.password);
+      user.createdAt = user.updatedAt = new Date();
+      return user;
+    });
     await queryInterface.bulkInsert("Users", data);
   },
 
