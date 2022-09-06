ca6374ee8bc0a02c10766494e6ab32e28c4ae934
SyahidbinFadlyl <ava.rageman18@gmail.com>
2022-09-06 22:37:14 +0700

    Feat : Update and Delete Transaction

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const user = require("./user");
+const transactions = require("./transactions");
 
 router.use("/users", user);
+router.use("/transactions", transactions);
 
 module.exports = router;
