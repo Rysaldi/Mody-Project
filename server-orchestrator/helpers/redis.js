92bf26c41d2da619649c60b7e4556f1d0c9542f6
Rio Hanantyoko <rhanantyoko@gmail.com>
2022-09-08 20:53:41 +0700

    skeleton server-orchestrator

---
+++ server-orchestrator/helpers/redis.js
@@ -0,0 +1,13 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+const Redis = require("ioredis");
+const redis = new Redis({
+  port: 11516, // Redis port
+  host: "redis-11516.c98.us-east-1-4.ec2.cloud.redislabs.com", // Redis host
+  username: "default", // needs Redis >= 6
+  password: process.env.REDIS_PW,
+  db: 0, // Defaults to 0
+});
+
+module.exports = redis;
