6c04582ad54a61bf885c2b3f3bb51e6e4985ce7f
SyahidbinFadlyl <ava.rageman18@gmail.com>
2022-09-09 20:14:09 +0700

    Update create and read transaction

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -7,7 +7,7 @@ class TransactionsController {
 			const { id } = req.params;
 
 			let { name, amount, CategoryId, date } = req.body;
- 
+
 			const findTransactions = await Transaction.findByPk(id, { transaction: t, });
 
 			if (!findTransactions) {
@@ -16,7 +16,7 @@ class TransactionsController {
 			const findCategory = await Category.findByPk(findTransactions.CategoryId, { transaction: t, });
 
 			const findWallet = await Wallet.findByPk(findTransactions.WalletId, { transaction: t, });
-			
+
 			if (findCategory.type === "Income") {
 				await Wallet.update({
 					totalAmount: findWallet.totalAmount - findTransactions.amount
@@ -110,38 +110,85 @@ class TransactionsController {
 	}
 
 	static async createTransaction(req, res, next) {
+		const t = await sequelize.transaction();
 		try {
+			console.log("masuk create");
 			const { name, amount, date, CategoryId, WalletId } = req.body;
+			const createTransaction = await Transaction.create(
+				{
+					name,
+					amount,
+					date,
+					UserId: req.user.id,
+					CategoryId,
+					WalletId,
+				},
+				{
+					returning: true,
+					transaction: t,
+				});
+			const findCategory = await Category.findByPk(CategoryId, { transaction: t, });
 
-			const { id: UserId } = req.user;
-			const transaction = await Transaction.create({
-				name,
-				amount,
-				date,
-				UserId,
-				CategoryId,
-				WalletId,
-			});
+			const findWallet = await Wallet.findByPk(WalletId, { transaction: t, });
+
+			if (findCategory.type === "Income") {
+				await Wallet.update({
+					totalAmount: findWallet.totalAmount + Number(amount)
+				}, {
+					where: {
+						id: findWallet.id
+					},
+					transaction: t
+				});
+			} else if (findCategory.type === "Expense") {
+				await Wallet.update({
+					totalAmount: findWallet.totalAmount - Number(amount)
+				}, {
+					where: {
+						id: findWallet.id
+					},
+					transaction: t
+				});
+			}
+
+			await t.commit();
 			res.status(201).json({
-				message: "Success Create Data",
-				transaction,
+				message: "Success create new Transaction",
+				Transaction: createTransaction
 			});
 		} catch (error) {
 			console.log(error);
+			await t.rollback();
 			next(error);
 		}
 	}
 
 	static async getTransaction(req, res, next) {
 		try {
-			const transaction = await Transaction.findAll({ include: [Category, Wallet] });
-			res.status(200).json({ transaction });
+			const { WalletId } = req.body;
+			const findTransactions = await Transaction.findAll({
+				include: [
+					{
+						model: Category,
+					},
+				],
+				order: [
+					['id', 'DESC']
+				],
+				where: {
+					WalletId: WalletId
+				}
+			});
+			if (findTransactions.length === 0) {
+				throw { name: "TransactionsNotFound" };
+			}
+			res.status(200).json(findTransactions);
 		} catch (error) {
 			next(error);
 		}
 	}
 
-	
+
 }
 
 module.exports = TransactionsController;
--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -3,10 +3,10 @@ const transactions = require("express").Router();
 const { authorizationTransactionRole } = require("../middlewares/authorization");
 
 
-// transactions.get("/", TransactionsController.getTransaction)
-// transactions.post("/", TransactionsController.createTransaction)
-transactions.delete("/:id",authorizationTransactionRole, TransactionsController.deleteTransaction);
-transactions.put("/:id",authorizationTransactionRole, TransactionsController.updateTransaction);
+transactions.get("/", TransactionsController.getTransaction);
+transactions.post("/", TransactionsController.createTransaction);
+transactions.delete("/:id", authorizationTransactionRole, TransactionsController.deleteTransaction);
+transactions.put("/:id", authorizationTransactionRole, TransactionsController.updateTransaction);
 
 
 module.exports = transactions;
