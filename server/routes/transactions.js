3e944468dcfbabaa5806e35138c646e5f35f4a6f
Rio Hanantyoko <rhanantyoko@gmail.com>
2022-09-09 20:06:31 +0700

    testing UD Wallet done

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,8 +1,8 @@
 const TransactionsController = require("../controllers/transaction");
 const transactions = require("express").Router();
 
-transactions.get("/", TransactionsController.getTransaction)
-transactions.post("/", TransactionsController.createTransaction)
+// transactions.get("/", TransactionsController.getTransaction)
+// transactions.post("/", TransactionsController.createTransaction)
 transactions.delete("/:id", TransactionsController.deleteTransaction);
 transactions.put("/:id", TransactionsController.updateTransaction);
 
