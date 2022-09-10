83fe0eed0430b83446dec0eb1c7e88408a9a1665
rysaldiwijaya <rysaldii@gmail.com>
2022-09-10 19:03:12 +0700

    added profile testing

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,42 +1,43 @@
 const { Profile } = require("../models");
 
 class ProfileController {
-  static async getOneProfile(req, res, next) {
-    try {
-      const oneProfile = await Profile.findOne({
-        where: {
-          UserId: req.user.id,
-        },
-      });
-      if (oneProfile) {
-        res.status(200).json(oneProfile);
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
+	static async getOneProfile(req, res, next) {
+		try {
+			const oneProfile = await Profile.findOne({
+				where: {
+					UserId: req.user.id,
+				},
+			});
+			if (oneProfile) {
+				res.status(200).json(oneProfile);
+			}
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  static async updateProfile(req, res, next) {
-    try {
-      const { firstName, lastName, profilePicture, phone } = req.body;
+	static async updateProfile(req, res, next) {
+		try {
+			const { firstName, lastName, profilePicture, phone } = req.body;
 
-        try {
-          const updateProfile = await Profile.update({
-            firstName,
-            lastName,
-            profilePicture,
-            phone,
-          }, {where:{
-            UserId:req.user.id
-          }});
-          if (updateProfile) res.status(200).json("update profile success");
-        } catch (error) {
-          next(error);
-        }
-    } catch (error) {
-      next(error);
-    }
-  }
+			const updateProfile = await Profile.update(
+				{
+					firstName,
+					lastName,
+					profilePicture,
+					phone,
+				},
+				{
+					where: {
+						UserId: req.user.id,
+					},
+				}
+			);
+			if (updateProfile) res.status(200).json({ message: "update profile success" });
+		} catch (error) {
+			next(error);
+		}
+	}
 }
 
 module.exports = ProfileController;
